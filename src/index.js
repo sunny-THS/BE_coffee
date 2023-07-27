827c1466cd79e5848f8c16947db65a61124735d5
sunny-THS <tuhueson@gmail.com>
2023-07-27 11:41:59 +0700

    sd

--- src/index.js
+++ src/index.js
@@ -11,25 +11,34 @@ const port = process.env.PORT || 5001;
 const app = express();
 
 // connect to mongodb
-mongoose.connect(process.env.COFFEE_DATA, () => console.log('Connected to mongodb'))
+mongoose.connect(process.env.COFFEE_DATA)
+const connDB = mongoose.connection
+connDB.on('error', (error) => {
+    console.log(error)
+})
+
+connDB.once('open', () => {
+    console.log(`MongoDB Connected: ${connDB.host}`.yellow.bold.underline)
+})
 
 console.log(process.env.COFFEE_DATA);
 
 // routes
-app.get('/api', async (req, res, next) =>  { 
-    let productions = await Product.find().exec();
-    const listtype = ['nc','bia','ta','gao'];
-    let data =  listtype.map( type => { 
-        return {
-            "type": type,
-            "data": productions.filter(product => product.type == type)
-        }
-    });
-    // console.log(productions);
-  res.status(res.statusCode || 200);
-    res.json({
-        product : data,
-    });
+app.get('/api', async (req, res) =>  { 
+    try {
+        const data = await Product.find().exec();
+        // const listtype = ['nc','bia','ta','gao'];
+        // let data =  listtype.map( type => { 
+        //     return {
+        //         "type": type,
+        //         "data": productions.filter(product => product.type == type)
+        //     }
+        // });
+        res.render('home', { data });
+    } catch (err) {
+        console.error('Error retrieving items from MongoDB:', err);
+        return res.status(500).send('Internal Server Error');
+    }
 });
 
 app.use((req, res, next) => {
